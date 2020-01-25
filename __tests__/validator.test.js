e2f3007ee64f7022be60e1751415aa0356a65d04
Nsuliman <nsuliman88@gmail.com>
2020-01-25 19:03:17 +0200

    factored Validator testing Completed

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,80 +1,154 @@
-'use strict';
+// 'use strict';
 
-const validator = require('../validator.js');
+// const validator = require('../../validator/validator.js');
 
-describe('validator module performs basic validation of', () => {
+// describe('validator module performs basic validation of', () => {
 
-  // TODO: Make this series of tests less repetitive ... DRY it out
+//   let str = 'yes';
+//   let num = 1;
+//   let arr = ['a'];
+//   let obj = {x:'y'};
+//   let func = () => {};
+//   let bool = false;
+
+//   it('strings', () => {
+//     expect(validator.isString(str)).toBeTruthy();
+//   });
+
+//   it('numbers', () => {
+//     expect(validator.isNumber(num)).toBeTruthy();
+//   });
+
+//   it('arrays', () => {
+//     expect(validator.isArrOrNot(arr)).toBeTruthy();
+//   });
+
+//   it('objects', () => {
+//     expect(validator.isObject(obj)).toBeTruthy();
+//   });
+
+//   it('booleans', () => {
+//     expect(validator.isBool(bool)).toBeTruthy();
+//   });
+
+//   it('functions', () => {
+//     expect(validator.isFunct(func)).toBeTruthy();
+//   });
+
+// });
+
+// describe('validator module performs complex validations', () => {
+
+//     const objectTest = {
+//         "firstName": "Fred",
+//         "lastName": "Sample",
+//         "hair": {
+//         "type": "wavy",
+//         "color": "brown"
+//         },
+//         "favoriteFoods": [
+//         "pizza",
+//         "cupcakes",
+//         "salmon"
+//         ],
+//         "married": true,
+//         "kids": 3
+//         }
+
+//   it('validates the presence of required object properties at any level', () => {
+//     // i.e. does person.hair.color exist and have a good value, not just person.hair
+//     expect(validator.objectLevels(objectTest)).toBeFalsy();
+//   });
+
+//   it('validates the proper types of object properties', () => {
+//     // i.e. person.name must be a string, etc.
+//     expect(validator.inputKeys(objectTest)).toBeTruthy();
+//   });
+
+//   it('validates the types of values contained in an array', () => {
+//     // i.e. an array of all strings or numbers
+//     expect(validator.arrayValue(objectTest)).toBeFalsy();
+//   });
+
+//   it('validates a value array against an approved list', () => {
+//     // i.e. a string might only be allowed to be "yes" or "no"
+//     expect(validator.emptyArray(objectTest)).toBeFalsy();
+//   });
+
+// });
+
+const ValidatorClass = require('../validator.js');
+
+describe('Validator Class', () => {
+
 
   let str = 'yes';
   let num = 1;
   let arr = ['a'];
-  let obj = {x:'y'};
-  let func = () => {};
+  let obj = { x: 'y' };
+  let func = () => { };
   let bool = false;
 
-  it('strings', () => {
-    expect(validator.isString(str)).toBeTruthy();
+  let val = new ValidatorClass(str, num, arr, obj, func, bool);
+
+  it('Strings ', () => {
+    expect(val.isString(str)).toBeTruthy();
   });
 
-  it('numbers', () => {
-    expect(validator.isNumber(num)).toBeTruthy();
+  it('Number ', () => {
+    expect(val.isNumber(num)).toBeTruthy();
   });
 
-  it('arrays', () => {
-    expect(validator.isArrOrNot(arr)).toBeTruthy();
+  it('Array ', () => {
+    expect(val.isArrOrNot(arr)).toBeTruthy();
   });
 
-  it('objects', () => {
-    expect(validator.isObject(obj)).toBeTruthy();
+  it('Object ', () => {
+    expect(val.isObject(obj)).toBeTruthy();
   });
 
-  it('booleans', () => {
-    expect(validator.isBool(bool)).toBeTruthy();
+  it('Func ', () => {
+    expect(val.isFunct(func)).toBeTruthy();
   });
 
-  it('functions', () => {
-    expect(validator.isFunct(func)).toBeTruthy();
+  it('Boolean ', () => {
+    expect(val.isBool(bool)).toBeTruthy();
   });
 
-});
-
-describe('validator module performs complex validations', () => {
-
-    const objectTest = {
-        "firstName": "Fred",
-        "lastName": "Sample",
-        "hair": {
-        "type": "wavy",
-        "color": "brown"
-        },
-        "favoriteFoods": [
-        "pizza",
-        "cupcakes",
-        "salmon"
-        ],
-        "married": true,
-        "kids": 3
-        }
+  let person = {
+    "firstName": "Fred",
+    "lastName": "Sample",
+    "hair": {
+      "type": "wavy",
+      "color": "brown"
+    },
+    "favoriteFoods": [
+      "pizza",
+      "cupcakes",
+      "salmon"
+    ],
+    "married": true,
+    "kids": 3
+  }
 
   it('validates the presence of required object properties at any level', () => {
     // i.e. does person.hair.color exist and have a good value, not just person.hair
-    expect(validator.objectLevels(objectTest)).toBeFalsy();
+    expect(val.objectLevels(person)).toBeFalsy();
   });
 
   it('validates the proper types of object properties', () => {
     // i.e. person.name must be a string, etc.
-    expect(validator.inputKeys(objectTest)).toBeTruthy();
+    expect(val.inputKeys(person)).toBeTruthy();
   });
 
   it('validates the types of values contained in an array', () => {
     // i.e. an array of all strings or numbers
-    expect(validator.arrayValue(objectTest)).toBeFalsy();
+    expect(val.arrayValue(person)).toBeFalsy();
   });
 
   it('validates a value array against an approved list', () => {
     // i.e. a string might only be allowed to be "yes" or "no"
-    expect(validator.emptyArray(objectTest)).toBeFalsy();
+    expect(val.emptyArray(person)).toBeFalsy();
   });
 
 });
--- validator.js
+++ validator.js
@@ -1,154 +1,70 @@
 'use strict';
 
-let validator = module.exports = {};
+/************************************** Refactoring Validator.js ***********************************/
 
-/**
- * Based on a set of rules, is the input valid?
- * TODO: Define the rules ... how do we send them in? How do we identify?
- * @param input
- * @param rules
- * @returns {boolean}
- */
-validator.isValid = (input, rules) => {
-  return true;
-};
+class ValidObject {
 
-/**
- * Is this a string?
- * @param input
- * @returns {boolean}
- */
-validator.isString = (input) => {
-  return typeof input === 'string';
-};
+  constructor(str, num, arr, obj, func, bool) {
 
-/**
-* Is this a string?
-* @param input
-* @returns {boolean}
-*/
-validator.isNumber = (input) => {
-  return typeof input === 'number';
-};
-
-/**
-* Is this a string?
-* @param input
-* @returns {boolean}
-*/
-validator.isArrOrNot = (input) => {
-  return Array.isArray(input);
-};
-
-/**
-* Is this a string?
-* @param input
-* @returns {boolean}
-*/
-validator.isObject = (input) => {
-  return typeof input === 'object';
-};
-
-/**
-* Is this a string?
-* @param input
-* @returns {boolean}
-*/
-validator.isBool = (input) => {
-  return typeof input === 'boolean';
-};
-
-/**
-* Is this a string?
-* @param input
-* @returns {boolean}
-*/
-validator.isFunct = (input) => {
-  return typeof input === 'function';
-};
-
-
-/** 
-* This is Function 
-* Is this an Object has properties at any level  ?
-* @param input
-* @returns {boolean}
-*/
-validator.objectLevels = function (input) {
-  let mainObj = Object.keys(input);
-  if (mainObj) {
-    mainObj.every(value => {
-      return this.isObject(value);
-    })
+    this.str = str;
+    this.num = num;
+    this.arr = arr;
+    this.obj = obj;
+    this.func = func;
+    this.bool = bool;
   }
-  return false;
-};
 
-/**
-* This is Function 
-* Is Object have A keys or Null?
-* @param input
-* @returns {boolean}
-*/
-validator.inputKeys = function (input) {
-  if (Object.keys(input)) { return true };
-};
+  isValid(input, rules) {
+    return true;
+  };
 
-/**
-* This is Function 
-* Is Object has an array value?
-* @param input
-* @returns value
-*/
-validator.arrayValue = function (input) {
-  // let obj = Object.values(input);
-  if (!Object.values(input)) { return false };
-  return Object.values(input).every(value => Array.isArray(value));
-};
+  isString(input) {
+    return typeof input === 'string';
+  };
 
-validator.emptyArray = function (input) {
-  // let obj = Object.values(input);
-  if (!Object.values(input)) { return false };
-  return Object.values(input).every(value => Array.isArray(value) && value.length === 0)
-};
+  isNumber(input) {
+    return typeof input === 'number';
+  };
 
-// /************************************** Refactoring Validator.js ***********************************/
+  isArrOrNot(input) {
+    return Array.isArray(input);
+  };
 
-// class ValidObject {
+  isObject(input) {
+    return typeof input === 'object';
+  };
 
-//   constructor(str, num, arr, obj, func, bool) {
+  isBool(input) {
+    return typeof input === 'boolean';
+  };
 
-//     this.str = str;
-//     this.num = num;
-//     this.arr = arr;
-//     this.obj = obj;
-//     this.func = func;
-//     this.bool = bool;
-//   }
+  isFunct(input) {
+    return typeof input === 'function';
+  };
 
-//   objectLevels(input) {
-//     let mainObj = Object.keys(input);
-//     if (mainObj) {
-//       mainObj.every(value => {
-//         return this.isObject(value);
-//       })
-//     }
-//     return false;
-//     }
+  objectLevels(input) {
+    let mainObj = Object.keys(input);
+    if (mainObj) {
+      mainObj.every(value => {
+        return this.isObject(value);
+      })
+    }
+    return false;
+  }
 
-//   inputKeys(input) {
-//     if (Object.keys(input)) { return true };
-//     return false;
-//     }
+  inputKeys(input) {
+    if (Object.keys(input)) { return true };
+  }
 
-//   arrayValue(input) {
-//     if (!Object.values(input)) { return false };
-//     return Object.values(input).every(value => Array.isArray(value));
-//   }
+  arrayValue(input) {
+    if (!Object.values(input)) { return false };
+    return Object.values(input).every(value => Array.isArray(value));
+  }
 
-//   emptyArray(input){
-//     if (!Object.values(input)) { return false };
-//     return Object.values(input).every(value => Array.isArray(value) && value.length === 0)
-//   }
-// } // end of Validator Class
+  emptyArray(input) {
+    if (!Object.values(input)) { return false };
+    return Object.values(input).every(value => Array.isArray(value) && value.length === 0)
+  }
+} // end of ValidObject Class
 
+module.exports = ValidObject;
