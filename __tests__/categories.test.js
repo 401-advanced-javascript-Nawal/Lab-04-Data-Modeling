db43b8b507ae3e2c0a8865c6e76299eeabc6d64a
Nsuliman <nsuliman88@gmail.com>
2020-01-25 02:37:59 +0200

    update test Done

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -17,7 +17,7 @@ describe('Categories Model', () => {
         });
       })
       .catch(e => console.error('ERR', e));
-  });
+  }); // end of create method 
 
   it('can get() a category', () => {
     let obj = { name: 'Test Category' };
@@ -29,7 +29,27 @@ describe('Categories Model', () => {
               expect(category[0][key]).toEqual(obj[key]);
             });
           });
-      });
-  });
+      })
+      .catch(e => console.error('ERR', e));
+  }); // end of get method 
 
-});
+  it('can update() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        return categories.update(record._id,record)
+          .then(category => {
+            Object.keys(obj).forEach(key => {
+              expect(category[key]).toEqual(obj[key]);
+            })
+          })
+      });
+  }); // end of update method 
+
+  it('can delete() a category', () => {
+
+
+  }); // end of delete method 
+
+
+}); // end of Categories Model
